a4323ef4f790d9a07e482a603dd18b84934c10e3
rinox3005 <peppe.rinox@gmail.com>
2024-06-24 18:32:17 +0200

    [UPD] handled multiple genres and single genre push - BONUS 1 DONE

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ class Movie
     private string $title;
     private string $director;
     private int $year;
-    private string $genre;
+    private array $genres;
     private string $originalLanguage;
     private int $duration;
 
     // Definizione del costruttore
-    public function __construct(string $title, string $director, int $year, string $genre, string $originalLanguage, int $duration)
+    public function __construct(string $title, string $director, int $year, array $genres, string $originalLanguage, int $duration)
     {
         $this->title = $title;
         $this->director = $director;
         $this->year = $year;
-        $this->genre = $genre;
+        $this->genres = $genres;
         $this->originalLanguage = $originalLanguage;
         $this->duration = $duration;
     }
@@ -37,9 +37,9 @@ class Movie
         return $this->year;
     }
 
-    public function getGenre(): string
+    public function getGenres(): array
     {
-        return $this->genre;
+        return $this->genres;
     }
 
     public function getOriginalLanguage(): string
@@ -71,9 +71,9 @@ class Movie
         $this->year = $year;
     }
 
-    public function setGenre(string $genre): void
+    public function setGenres(array $genres): void
     {
-        $this->genre = $genre;
+        $this->genres = $genres;
     }
 
     public function setOriginalLanguage(string $originalLanguage): void
@@ -89,30 +89,45 @@ class Movie
         $this->duration = $duration;
     }
 
+    public function addGenre(string $genre): void
+    {
+        array_push($this->genres, $genre);
+    }
+
+
     // Definizione di un metodo per ottenere le informazioni del film
     public function getMovieInfo(): string
     {
+        $genresStr = implode(", ", $this->getGenres());
         return "Title: " . $this->getTitle() . "<br>" .
             "Director: " . $this->getDirector() . "<br>" .
             "Year: " . $this->getYear() . "<br>" .
-            "Genre: " . $this->getGenre() . "<br>" .
+            "Genres: " . $genresStr . "<br>" .
             "Original Language: " . $this->getOriginalLanguage() . "<br>" .
             "Duration: " . $this->getDuration() . " min";
     }
 }
 
+// Utilizzo della classe Movie con gestione delle eccezioni
 try {
-    // Creazione della prima istanza della classe 'Movie'
-    $movie1 = new Movie("Interstellar", "Christopher Nolan", 2010, "Science Fiction", "English", 148);
+    // Creazione della prima istanza della classe 'Movie' con aggiunta di generi multipli
+    $genres1 = ["Action", "Sci-Fi"];
+    $movie1 = new Movie("Interstellar", "Christopher Nolan", 2010, $genres1, "English", 148);
 
-    // Creazione della seconda istanza della classe 'Movie'
-    $movie2 = new Movie("The Matrix", "The Wachowskis", 1999, "Action", "English", 136);
+    // Creazione della seconda istanza della classe 'Movie' con aggiunta di generi multipli
+    $genres2 = ["Drama", "Thriller"];
+    $movie2 = new Movie("The Matrix", "The Wachowskis", 1999, $genres2, "English", 136);
+
+    // Aggiungo un singolo genere al film
+    $movie1->addGenre('Crime');
 
     // Stampa delle informazioni del primo film
     echo "First Movie Info:<br>" . $movie1->getMovieInfo();
 
     // Stampa delle informazioni del secondo del film
     echo "<br><br>Second Movie Info:<br>" . $movie2->getMovieInfo();
-} catch (Exception $e) {
+}
+// Gestisco l'errore
+catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
